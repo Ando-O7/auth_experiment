a8c5592d191326f90a31bd573087d013b82c3a00
ando07 <ando.masahito@gmail.com>
2019-05-19 16:52:02 +0900

    [add]firebase config.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: 'KEY',
+  authDmain: 'DOMAIN.firebaseapp.com',
+  databaseURL: 'DOMAIN.firebaseio.com',
+  projectId: 'ID',
+  storageBucket: 'BUCKET_ID.appspot.com',
+  messageingSenderId: 'SENDER_ID'
+}
+firebase.initializeApp(config)
 
 new Vue({
   router,
